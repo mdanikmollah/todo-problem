8593d22bf15e045bfe784e54894ae6fcbd2a900a
Mohammad Anik Mollah <mdanik0178@gmail.com>
2024-02-20 13:27:00 +0600

    ar na

--- js/script.js
+++ js/script.js
@@ -4,6 +4,7 @@ let error = document.querySelector(".error")
 let ol = document.querySelector("ol")
 let olerror = document.querySelector(".ol_error")
 let todoarr = []
+let arrindex;
 
 btn.addEventListener("click",function(){
     if (inputbox.value == ""){
@@ -11,17 +12,25 @@ btn.addEventListener("click",function(){
 
         
     }else{
-        error.innerHTML = ""
-        todoarr.push(inputbox.value)
-        inputbox.value = ""
-        todo()
+        if (btn.innerHTML == "Add") {
+            error.innerHTML = ""
+            todoarr.push(inputbox.value)
+            inputbox.value = ""
+            todo()
+            
+        }else{
+            todoarr[arrindex] = inputbox.value
+            todo()
+            btn.innerHTML = "Add"
+            inputbox.value = ""
+        }
     }
     //console.log(todoarr);
 })
 function todo(){
     ol.innerHTML = ""
     todoarr.map((item)=>{
-    ol.innerHTML += `<li>${item}<button>Edit</button><button class="delete">Delete</button></li>`
+    ol.innerHTML += `<li>${item}<button class="edit">Edit</button><button class="delete">Delete</button></li>`
         //delete operation
     let deletebtn = document.querySelectorAll(".delete")
     let deletebtnArr = Array.from(deletebtn)
@@ -39,7 +48,8 @@ function todo(){
     editbtnArr.map((edititem,index)=>{
         edititem.addEventListener("click",function() {
             btn.innerHTML = "Update"
-            inputbox.value = todoarr(index)
+            inputbox.value = todoarr[index]
+            arrindex = index
             
         })    
         
